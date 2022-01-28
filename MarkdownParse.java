2544810246111e5f7b7dceca6b86b3a2105aef16
annaz0506 <annaz0506@icloud.com>
2022-01-27 18:36:24 -0800

    Lab Report 2 Changes #1

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,11 +12,16 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            if(markdown.substring(nextOpenBracket-1, nextOpenBracket).contains("!")==false){
+                int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+                int openParen = markdown.indexOf("(", nextCloseBracket);
+                int closeParen = markdown.indexOf(")", openParen);
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+            }
+            else{
+                return toReturn;
+            }
         }
         return toReturn;
     }
---
+++ lab-test3.md
@@ -0,0 +1,3 @@
+# Title
+
+[link?]www.hi.com
