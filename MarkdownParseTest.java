f12ea670ca50487120b4e9208362959933474bc9
annaz0506 <annaz0506@icloud.com>
2022-02-09 18:56:17 -0800

    Updates

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,29 +1,28 @@
 import static org.junit.Assert.*;
-
+import org.junit.*;
+ 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-
-import org.junit.*;
-
+ 
+ 
+//javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
+//java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
 public class MarkdownParseTest {
-    @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
-    }
-
-    @Test
-    public void testFileOne() throws IOException{
-        String contentsTest = Files.readString(Path.of("test-file.md"));
-        assertEquals(List.of("https://something.com", "some-page.html"), 
-                MarkdownParse.getLinks(contentsTest));
-    }
-
-    @Test
-    public void testFileTwo() throws IOException{
-        String contentsTest = Files.readString(Path.of("lab.md"));
-        assertEquals(List.of("https://anything.com", "another-page.html", "another-page.html", 
-                "a-page.html"), MarkdownParse.getLinks(contentsTest));
-    }
+   @Test
+   public void testFiles() throws IOException { 
+       String contentsTest = Files.readString(Path.of("test-file.md"));
+       String contentsTest1 = Files.readString(Path.of("test-file2.md"));
+       String contentsTest2 = Files.readString(Path.of("test-file3.md"));
+ 
+       assertEquals(List.of("https://something.com", "some-page.html"),
+           MarkdownParse.getLinks(contentsTest));
+       assertEquals(List.of("https://anything.com"),
+           MarkdownParse.getLinks(contentsTest1));
+       assertEquals(List.of("weird-page.html"),
+           MarkdownParse.getLinks(contentsTest2));
+ 
 }
+}
+
---
+++ test-file3.md
@@ -0,0 +1,1 @@
+[link](weird-page.html) 
